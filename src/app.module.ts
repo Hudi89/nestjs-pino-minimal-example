381eec6088025ea308eae9c4b0a0f3291284932b
Peter Hudoba <hudi1989@gmail.com>
2022-06-01 20:22:34 +0200

    IDK

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from 'nestjs-pino';
+import pino from 'pino';
+
+const logger = pino({
+  level: 'debug',
+});
 
 @Module({
-  imports: [LoggerModule.forRoot()],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: logger,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
   app.useLogger(app.get(Logger));
+  app.flushLogs();
   await app.listen(3000);
 }
 bootstrap();
